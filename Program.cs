6a937b7e2b9bb3a932bab8ada50c9eecf9b13166
VelizarZhulev <32514074+VelizarZhulev@users.noreply.github.com>
2017-10-04 20:23:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ingredients = Console.ReadLine();
+            var next = 0;
+
+            while (ingredients != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {ingredients}.");
+                ingredients = Console.ReadLine();
+                next++;
+            }
+            Console.WriteLine($"Preparing cake with {next} ingredients.");
+
+        }
+    }
+}
